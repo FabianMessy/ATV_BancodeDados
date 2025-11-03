eea46568ea787baf9242c0aa44230d48bb398fb3
Fabian <160931865+FabianMessy@users.noreply.github.com>
2025-11-03 08:17:48 -0300

    Add files via upload

---
+++ projetobancodedados/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from config import Config
+from models import db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
